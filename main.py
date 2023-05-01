91d56cdc085090a4a2642828362329c8095b405f
junyoungparkdev <sonsodock@gmail.com>
2023-05-01 11:52:17 +0900

    prnit("TestTest")

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 # Test env
 
-print('hello')
+def print_hello():
+    animals = ['dog','cat','hamster','tiger'] # in one line
+    foods = [
+	'Spagetti',
+	'Pizza',
+	'bibimbob'
+    ] # w/0 trailing comma
+    names = [
+ 	'John',
+	'Jane',
+	'Gil-dong',
+	'Dong-eun',
+    ] # w/ trailing commag
+    for f_name in names:
+        print(f'hello, {f_name}')
+
+
+if __name__ == '__main__':
+    print_hello()
+
+
+
+print("Test")
+
